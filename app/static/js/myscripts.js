00563aa7eb0a337ebe8883ffc957051617693fb5
razerx100 <saikatmandal8@gmail.com>
2020-02-28 13:20:33 +0600

    js improvement

--- app/static/js/myscripts.js
+++ app/static/js/myscripts.js
@@ -1,20 +1,29 @@
+let page_ids = ["dashboard" , "categories", "manufacturer", "products", "orders"];
+let page_data_cls = [".dash_stuff", ".cat_stuff", ".manu_stuff", ".prod_stuff", ".order_stuff"];
+
+class Pages{
+    constructor(page_id, page_data_cls){
+        this.id = page_id;
+        this.data_cls = page_data_cls;
+    }
+}
+
+let objects = new Array(page_ids.length);
+
 $(document).ready(function(){
     $('#sidebarCollapse').on('click', toggle_sidebar);
 });
 
 $(document).ready(toggle_sidebar);
 
-$(document).ready(dashboard_show);
+$(document).ready(landing_view);
 
 $(document).ready(function(){
-    $('#dashboard').click(dashboard_show);
-});
-
-$(document).ready(function(){
-    $('#categories').click(function(){
-        $('.dash_stuff').hide();
-        $('.cat_stuff').show();
-    });
+    $("#dashboard").click(toggle_page_data);
+    $("#categories").click(toggle_page_data);
+    $("#manufacturer").click(toggle_page_data);
+    $("#products").click(toggle_page_data);
+    $("#orders").click(toggle_page_data);
 });
 
 function toggle_sidebar(){
@@ -22,7 +31,31 @@ function toggle_sidebar(){
     $(this).toggleClass('active');
 }
 
-function dashboard_show(){
-    $('.dash_stuff').show();
-    $('.cat_stuff').hide();
+function landing_view(){
+    for(i = 0; i < page_ids.length; i+=1){
+        objects[i] = new Pages(page_ids[i], page_data_cls[i]);
+    }
+    for(i = 0; i < objects.length; i+=1){
+        if("dashboard" == objects[i].id){
+            $(objects[i].data_cls).show();
+        }
+        else{
+            $(objects[i].data_cls).hide();
+        }
+    }
+}
+
+function toggle_page_data(){
+    let current_page = (this.id);
+    for(i = 0; i < page_ids.length; i+=1){
+        objects[i] = new Pages(page_ids[i], page_data_cls[i]);
+    }
+    for(i = 0; i < objects.length; i+=1){
+        if(current_page == objects[i].id){
+            $(objects[i].data_cls).show();
+        }
+        else{
+            $(objects[i].data_cls).hide();
+        }
+    }
 }
