841ecd712b145cdab76513677492d3a41e4146c1
razerx100 <saikatmandal8@gmail.com>
2020-03-24 22:32:48 +0600

    heroku test

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "Put a strong default key here"
     SQLALCHEMY_DATABASE_URI = (
-        os.environ.get("DATABASE_URL") or "mysql://razerx100:35401350@localhost/meds"
+        os.environ.get("DATABASE_URL")
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = "./app/static/img/"
